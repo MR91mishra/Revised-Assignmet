5d60e6510b817afbc6ef0817176e619e7abafc81
MR91mishra <90860540+MR91mishra@users.noreply.github.com>
2021-09-17 20:06:57 +0530

    Delete journy-ass directory

--- journy-ass/assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  winW =$(window).width();
-  //alert(winW);
-  let Mart = $('.journy__logo__right').outerHeight();
-  $(".journy__infinite__newsletter").css('margin-top', Mart);
-  $(".journy__logo__right img").css("max-width",winW/2 - 50);
-
-});
